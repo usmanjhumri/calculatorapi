926160271d2a98f0409873367c2f040fe6b8c9c6
usmanlatif123 <usman@enigmadevcraft.com>
2024-03-10 15:47:13 +0500

    add server code

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import json from "body-parser";
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(json());
+
+app.post("/calculate", (req, res) => {
+  const { num1, num2, operator } = req.body;
+  let result;
+
+  switch (operator) {
+    case "addition":
+      result = num1 + num2;
+      break;
+    case "subtraction":
+      result = num1 - num2;
+      break;
+    case "multiplication":
+      result = num1 * num2;
+      break;
+    default:
+      result = "Invalid operator";
+  }
+
+  res.json({ result });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
