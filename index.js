e4a13384a78ffea9aaae992af21139eb2cf4f3d4
usmanlatif123 <usman@enigmadevcraft.com>
2024-03-11 12:34:26 +0500

    ui

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ const port = process.env.PORT || 3001;
 app.use(json());
 app.post("/calculate", (req, res) => {
   res.json(req.body);
-  const { num1, num2, operator } = req.body;
-  let result;
-  switch (operator) {
-    case "addition":
-      result = num1 + num2;
-      break;
-    case "subtraction":
-      result = num1 - num2;
-      break;
-    case "multiplication":
-      result = num1 * num2;
-      break;
-    default:
-      result = "Invalid operator";
-  }
-  res.json({ result });
+  // const { num1, num2, operator } = req.body;
+  // let result;
+  // switch (operator) {
+  //   case "addition":
+  //     result = num1 + num2;
+  //     break;
+  //   case "subtraction":
+  //     result = num1 - num2;
+  //     break;
+  //   case "multiplication":
+  //     result = num1 * num2;
+  //     break;
+  //   default:
+  //     result = "Invalid operator";
+  // }
+  // res.json({ result });
 });
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
